9a0721bd5cbfd455aacddfa591fbea3ddee614ae
Camilius <Kamil.Zabkiewicz@gmail.com>
2025-03-26 01:00:24 +0200

    Update arr_size2.c

--- arr_size2.c
+++ arr_size2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 100
+#define SIZE 512
 
-void fillArray(int* arr){
+void fillArrayI(int* arr){
   srand(time(NULL));
   int size = arr[0];
   int i;
@@ -12,7 +12,7 @@ void fillArray(int* arr){
   }
 }
 
-void printArray(int* arr){
+void printArrayI(int* arr){
   
   int size = arr[0];
   int i;
@@ -22,17 +22,50 @@ void printArray(int* arr){
   
 }
 
+
+void fillArrayC(char* arr){
+  srand(time(NULL));
+  int size = *((int*) arr);
+  int i;
+  for(i=sizeof(int);i<size+sizeof(int);i++){
+    arr[i]=rand()%94+33;
+  }
+}
+
+void printArrayC(char* arr){
+  
+  int size = *((int*) arr);
+  int i;
+  for(i=sizeof(int);i<size+sizeof(int);i++){
+    printf("arr[%d]=%c \n",i,arr[i]);
+  }
+  
+}
+
+void printArrayC2(char* arr){
+  printf("%s \n",arr);
+}
+
 int main()
 {
     int* arr;
+    char* arr2; 
     
     arr = (int*) malloc((SIZE+1)*sizeof(int));
     arr[0]=SIZE;
     
-    fillArray(arr);
-    printArray(arr);
+    arr2 = (char*) malloc((SIZE)*sizeof(char)+1*sizeof(int));
+    *((int *)arr2)=SIZE;
+    
+    //fillArrayI(arr);
+    //printArrayI(arr);
+    
+    fillArrayC(arr2);
+    printArrayC(arr2);
+    printArrayC2(arr2);
     
     free(arr);
+    free(arr2);
     
     return 0;
     //printf("Hello, World!");
